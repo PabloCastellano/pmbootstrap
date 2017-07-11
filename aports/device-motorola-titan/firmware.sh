db821b04c9a7a36c0474463950d89ccff002584e
Pablo Castellano <pablo@anche.no>
2017-07-12 00:57:28 +0200

    Different firmware.sh
    Adapted from:
    http://trac.clfs.org/browser/udev/firmware.sh?rev=7e161ea861087292da223fbbf49e9c495f681b8f

--- aports/device-motorola-titan/firmware.sh
+++ aports/device-motorola-titan/firmware.sh
@@ -1,29 +1,36 @@
-#!/bin/sh -e
+#!/bin/sh
+########################################################################
+#
+# Description : Firmware Script
+#
+# Authors     : Based on Open Suse Udev Rules
+#               kay.sievers@suse.de
+#
+# Adapted to  : Jim Gifford
+# LFS
+#
+# Version     : 00.00
+#
+# Notes       :
+#
+########################################################################
 
-FIRMWARE_DIRS="/lib/firmware/updates/$(uname -r) /lib/firmware/updates \
-               /lib/firmware/$(uname -r) /lib/firmware"
+FIRMWARE_DIRS="/lib/firmware"
 
-err() {
-	echo "$@" >&2
-	logger -t "${0##*/}[$$]" "$@" 2>/dev/null || true
-}
-
-if [ ! -e /sys$DEVPATH/loading ]; then
-	err "udev firmware loader misses sysfs directory"
-	exit 1
+if [ ! -e /sys/$DEVPATH/loading ]; then
+    echo "firmware loader misses sysfs directory" >> /root/firmwareload.log
+    exit 0
 fi
 
 for DIR in $FIRMWARE_DIRS; do
-	[ -e "$DIR/$FIRMWARE" ] || continue
-	echo 1 > /sys$DEVPATH/loading
-	cat "$DIR/$FIRMWARE" > /sys$DEVPATH/data
-	echo 0 > /sys$DEVPATH/loading
-	exit 0
+    [ -e "$DIR/$FIRMWARE" ] || continue
+    echo "loading $DIR/$FIRMWARE" >> /root/firmwareload.log
+    echo 1 > /sys/$DEVPATH/loading
+    cat "$DIR/$FIRMWARE" > /sys/$DEVPATH/data
+    echo 0 > /sys/$DEVPATH/loading
+    exit
 done
 
-echo -1 > /sys$DEVPATH/loading
-err "Cannot find  firmware file '$FIRMWARE'"
-mkdir -p /dev/.udev/firmware-missing
-file=$(echo "$FIRMWARE" | sed 's:/:\\x2f:g')
-ln -s -f "$DEVPATH" /dev/.udev/firmware-missing/$file
+echo -1 > /sys/$DEVPATH/loading
+echo "Cannot find  firmware file '$FIRMWARE'" >> /root/firmwareload.log
 exit 1
