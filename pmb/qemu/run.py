facf61603ce9e414f7e7b0a6344aef44b09e8017
Pablo Castellano <pablo@anche.no>
2017-08-31 12:34:12 +0200

    PEP8

--- pmb/qemu/run.py
+++ pmb/qemu/run.py
@@ -81,7 +81,6 @@ def qemu_command(args, arch, device, img_path):
     command += ["-device", "e1000,netdev=net0"]
     command += ["-netdev", "user,id=net0,hostfwd=tcp::" + str(args.port) + "-:22"]
 
-
     if deviceinfo["dtb"] != "":
         dtb_image = rootfs + "/usr/share/dtb/" + deviceinfo["dtb"] + ".dtb"
         if not os.path.exists(dtb_image):
